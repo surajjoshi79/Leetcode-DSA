be31a3c098df31f53a4bec4bad50af48b684ae9a
Suraj Joshi <jtcoding1925@gmail.com>
2025-07-01 13:46:30 +0530

    Time: 14 ms (23.30%) | Memory: 60.5 MB (7.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -4,13 +4,6 @@ class Solution {
         for(int i:nums){
             set.add(i);
         }
-        /*int max=nums[0];
-        for(int i:nums){
-            max=Math.max(i,max);
-        }
-        if(max<=0){
-            return 1;
-        }*/
         for(int i=1;i<=nums.length;i++){
             if(!set.contains(i)){
                 return i;
