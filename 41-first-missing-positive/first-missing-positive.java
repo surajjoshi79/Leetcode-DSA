32ab35c41094edd8448800c01535469e8e31762d
Suraj Joshi <jtcoding1925@gmail.com>
2025-07-01 13:46:10 +0530

    Time: 13 ms (29.97%) | Memory: 60.8 MB (5.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -4,18 +4,18 @@ class Solution {
         for(int i:nums){
             set.add(i);
         }
-        int max=nums[0];
+        /*int max=nums[0];
         for(int i:nums){
             max=Math.max(i,max);
         }
         if(max<=0){
             return 1;
-        }
-        for(int i=1;i<=max;i++){
+        }*/
+        for(int i=1;i<=nums.length;i++){
             if(!set.contains(i)){
                 return i;
             }
         }
-        return max+1;
+        return nums.length+1;
     }
 }
