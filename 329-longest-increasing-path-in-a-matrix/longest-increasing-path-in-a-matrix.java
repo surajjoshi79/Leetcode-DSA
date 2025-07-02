16e85a3c6461d9c478b1abac6e56920313ba23f8
Suraj Joshi <jtcoding1925@gmail.com>
2025-07-02 12:29:17 +0530

    Time: 7 ms (97.02%) | Memory: 45.1 MB (64.67%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        int longest=1;
+        Integer[][] dp=new Integer[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                longest=Math.max(longest,helper(matrix,i,j,dp));
+            }
+        }
+        return longest;
+    }
+    int helper(int[][] matrix,int row,int col,Integer[][] dp){
+        if(row==matrix.length || col==matrix[0].length || row<0 || col<0){
+            return 0;
+        }
+        if(dp[row][col]!=null){
+            return dp[row][col];
+        }
+        int up=0,down=0,left=0,right=0;
+        if(row-1>=0 && matrix[row-1][col]>matrix[row][col]){
+            up=helper(matrix,row-1,col,dp);
+        }
+        if(row+1<matrix.length && matrix[row][col]<matrix[row+1][col]){
+            down=helper(matrix,row+1,col,dp);
+        }
+        if(col-1>=0 && matrix[row][col-1]>matrix[row][col]){
+            left=helper(matrix,row,col-1,dp);
+        }
+        if(col+1<matrix[0].length && matrix[row][col+1]>matrix[row][col]){
+            right=helper(matrix,row,col+1,dp);
+        }
+        return dp[row][col]=1+Math.max(Math.max(up,down),Math.max(left,right));
+    }
+}
