2235822fc2b84ac373255472355226e0aa4a5539
Suraj Joshi <jtcoding1925@gmail.com>
2025-07-03 14:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (81.95%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null){
+            TreeNode newNode=new TreeNode(val);
+            return newNode;
+        }
+        /*if(root.left==null && root.val>val){
+            TreeNode newNode=new TreeNode(val);
+            root.left=newNode;
+        }
+        if(root.right==null && root.val<val){
+            TreeNode newNode=new TreeNode(val);
+            root.right=newNode;
+        }*/
+        if(val>root.val){
+            root.right=insertIntoBST(root.right,val);
+        }
+        else{
+            root.left=insertIntoBST(root.left,val);
+        }
+        return root;
+    }
+}
