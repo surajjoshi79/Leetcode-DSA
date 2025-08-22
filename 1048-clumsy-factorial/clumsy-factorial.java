decce8aa691bf5765a23cc373ad45b16afb94a94
Suraj Joshi <jtcoding1925@gmail.com>
2025-08-22 09:07:19 +0530

    Time: 10 ms (49.89%) | Memory: 43.9 MB (37.20%) - LeetSync

---
+++ 1048-clumsy-factorial/clumsy-factorial.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int clumsy(int n) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(n);
+        n=n-1;
+        int index=0;
+        while(n>0){
+            if(index % 4==0){
+                stack.push(stack.pop() * n);
+            }else if(index % 4==1){
+                stack.push(stack.pop() / n);
+            }else if(index % 4==2){
+                stack.push(n);
+            }else{
+                stack.push(-n);
+            }
+            index++;
+            n--;
+        }
+        int result=0;
+        for(int i:stack){
+            result+=i;
+        }
+        return result;
+    }
+}
